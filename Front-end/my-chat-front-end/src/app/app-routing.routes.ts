17531c1350234bf6b90c3e190facdd4adab893d9
Elkana Sassi <elkanas123@gmail.com>
2025-07-17 16:13:38 +0300

    Fixed routing. Now routes directly to the login page.

--- Front-end/my-chat-front-end/src/app/app-routing.routes.ts
+++ Front-end/my-chat-front-end/src/app/app-routing.routes.ts
@@ -1,13 +1,9 @@
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
-import { SidebarComponent } from './layout/sidebar/sidebar.component';
-import { NgModule } from '@angular/core';
-import { LoginComponent } from './features/auth/login/login.component';
-import { RegisterComponent } from './features/auth/register/register.component';
 
 export const routes: Routes = [
     {
-        path: '',
+        path: 'layout',
         component: LayoutComponent,
         children: [
             {
@@ -20,7 +16,7 @@ export const routes: Routes = [
     {
         path: 'auth',
         loadChildren: () =>
-            import('./features/auth/auth-routing.module').then((m) => m.routes),
-    }, 
+            import('./features/auth/auth-routing.module').then((m) => m.AuthRoutes),
+    },
     { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
 ];
--- Front-end/my-chat-front-end/src/app/app.config.ts
+++ Front-end/my-chat-front-end/src/app/app.config.ts
@@ -2,11 +2,14 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app-routing.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { AuthRoutes } from './features/auth/auth-routing.module';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient()
   ]
 };
