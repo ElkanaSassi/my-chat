5efe747f7775ef8e78f56b2e7e1b0fe4c615ef57
Elkana Sassi <elkanas123@gmail.com>
2025-07-17 21:39:56 +0300

    Front-end: Added Http service. and deleted unnecessary files (spec.ts).

--- Front-end/my-chat-front-end/src/app/core/services/http/httpConnection.service.ts
+++ Front-end/my-chat-front-end/src/app/core/services/http/httpConnection.service.ts
@@ -1,12 +1,25 @@
 import { HttpClient } from "@angular/common/http";
-import { inject, Injectable } from "@angular/core";
-
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from '../../../env/environment';
 
 @Injectable({ providedIn: 'root' })
 export class HttpService {
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) { }
 
-    method(Data: string) {
+    get<T>(path: string, params?: any): Observable<T> {
+        return this.http.get<T>(`${environment.apiUrl}/${path}`, { params });
+    }
 
+    post<T>(path: string, body: any): Observable<T> {
+        return this.http.post<T>(`${environment.apiUrl}/${path}`, body);
+    }
+
+    put<T>(path: string, body: any): Observable<T> {
+        return this.http.put<T>(`${environment.apiUrl}/${path}`, body);
+    }
+
+    delete<T>(path: string): Observable<T> {
+        return this.http.delete<T>(`${environment.apiUrl}/${path}`);
     }
 }
